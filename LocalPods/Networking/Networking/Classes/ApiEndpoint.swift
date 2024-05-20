797795af83fdc40e6c44e7a04537be54a06a1c7f
Jiaxin Pu <jiaxin.pu@thoughtworks.com>
2024-05-20 14:19:53 +0800

    update IP

--- LocalPods/Networking/Networking/Classes/ApiEndpoint.swift
+++ LocalPods/Networking/Networking/Classes/ApiEndpoint.swift
@@ -19,7 +19,7 @@ extension ApiEndpoint: TargetType {
     public var baseURL: URL {
         switch StandardBaseUrlManager.instance.channel {
         case .aws:
-            return .init(string: "http://3.27.66.117:8080")!
+            return .init(string: "http://13.211.137.50:8080")!
         case .local:
             return .init(string: "http://localhost:8080")!
         }
